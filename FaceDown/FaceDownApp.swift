a1d14bb98d366e720d569c2809a65721ee8de8b8
William Huang <46873752+WilliamH141@users.noreply.github.com>
2025-10-03 15:33:54 +1300

    Initial Commit

---
+++ FaceDown/FaceDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceDownApp.swift
+//  FaceDown
+//
+//  Created by William Huang on 03/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FaceDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
